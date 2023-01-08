02676de4b2aabc7fe1cb942e05a2f5c4ae072d96
Sushant0999 <sushantraj215@gmail.com>
2023-01-08 18:57:43 +0530

    EASY

---
+++ Running_sum_of_1D_Array.java
@@ -0,0 +1,13 @@
+//link :- https://leetcode.com/problems/running-sum-of-1d-array/
+public class Running_sum_of_1D_Array {
+    public int[] runningSum(int[] nums) {
+        int sum = 0;
+        int n = nums.length;
+        int[] result = new int[n];
+        for(int i = 0; i < nums.length; i++){
+            sum = sum + nums[i];
+            result[i] = sum;
+        }
+        return result;
+    }
+}
