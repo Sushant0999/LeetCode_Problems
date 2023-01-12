be2e64d3df479388e5c322458c3b39f1d3c185c9
Sushant0999 <sushantraj215@gmail.com>
2023-01-12 23:04:46 +0530

    EASY

--- Running_sum_of_1D_Array.java
+++ Running_sum_of_1D_Array.java
@@ -3,7 +3,7 @@ public class Running_sum_of_1D_Array {
     public int[] runningSum(int[] nums) {
         int sum = 0;
         int n = nums.length;
-        int[] result = new int[n];
+        int[] result  = new int[n];
         for(int i = 0; i < nums.length; i++){
             sum = sum + nums[i];
             result[i] = sum;
